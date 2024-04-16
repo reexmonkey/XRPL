104ed3ecd1966127016b97df5adf7ddc76b32964
reexmonkey <ngwanemk@gmail.com>
2024-04-16 22:23:45 +0200

    Added Transaction entry response and dependencies

---
+++ XRPL.Core.Domain/Models/TransactionMetadata.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.Serialization;
+using XRPL.Core.Domain.Entries;
+
+namespace XRPL.Core.Domain.Models
+{
+    /// <summary>
+    /// Represents the section of data that gets added to a transaction after it is processed.
+    /// <para/> Any transaction that gets included in a ledger has metadata, regardless of whether it is successful. The transaction metadata describes the outcome of the transaction in detail.
+    /// <para/> The changes described in transaction metadata are only final if the transaction is in a validated ledger version.
+    /// </summary>
+    [KnownType(typeof(AccountRoot))]
+    [KnownType(typeof(AMM))]
+    [KnownType(typeof(Ammendments))]
+    [KnownType(typeof(Bridge))]
+    [KnownType(typeof(CurrencyAmountCheck))]
+    [KnownType(typeof(XRPCheck))]
+    [KnownType(typeof(DepositPreauth))]
+    [KnownType(typeof(DID))]
+    [KnownType(typeof(DirectoryNode))]
+    [KnownType(typeof(Escrow))]
+    [KnownType(typeof(FeeSettings))]
+    [KnownType(typeof(LedgerHashes))]
+    [KnownType(typeof(NegativeUNL))]
+    [KnownType(typeof(XrpForNFTokenOffer))]
+    [KnownType(typeof(CurrencyAmountForNFTokenOffer))]
+    [KnownType(typeof(NFTokenPage))]
+    [KnownType(typeof(XrpForCurrencyAmountOffer))]
+    [KnownType(typeof(CurrencyAmountForXrpOffer))]
+    [KnownType(typeof(CurrencyAmountForCurrencyAmountOffer))]
+    [KnownType(typeof(PayChannel))]
+    [KnownType(typeof(RippleState))]
+    [KnownType(typeof(SignerList))]
+    [KnownType(typeof(Ticket))]
+    [KnownType(typeof(XRPXChainOwnedClaimID))]
+    [KnownType(typeof(CurrencyAmountXChainOwnedClaimID))]
+    [KnownType(typeof(XrpXChainOwnedCreateAccountClaimID))]
+    [KnownType(typeof(CurrencyAmountXChainOwnedCreateAccountClaimID))]
+    public class TransactionMetadata
+    {
+        public LedgerEntryBase[]? AffectedNodes { get; set; }
+
+        /// <summary>
+        /// (May be omitted) For a partial payment, this field records the amount of currency actually delivered to the destination.
+        /// <para/>To avoid errors when reading transactions, instead use the delivered_amount field, which is provided for all Payment transactions, partial or not.
+        /// </summary>
+        public CurrencyAmount[]? CurrencyAmounts { get; set; }
+
+        /// <summary>
+        /// (May be omitted) For a partial payment, this field records the amount of currency actually delivered to the destination.
+        /// <para/>To avoid errors when reading transactions, instead use the delivered_amount field, which is provided for all Payment transactions, partial or not.
+        /// </summary>
+        [DataMember(Name = "DeliveredAmount")]
+        public CurrencyAmount[]? PartialPaymentDeliveredAmount { get; set; }
+
+        /// <summary>
+        /// The transaction's position within the ledger that included it. This is zero-indexed. (For example, the value 2 means it was the 3rd transaction in that ledger.)
+        /// </summary>
+        public uint TransactionIndex { get; set; }
+
+        /// <summary>
+        /// A result code indicating whether the transaction succeeded or how it failed.
+        /// </summary>
+        public string? TransactionResult { get; set; }
+
+        /// <summary>
+        /// (Omitted for non-Payment transactions) The [Currency Amount][] actually received by the Destination account. Use this field to determine how much was delivered, regardless of whether the transaction is a partial payment. See this description for details.
+        /// </summary>
+        [DataMember(Name = "delivered_amount")]
+        public CurrencyAmount? DeliveredAmount { get; set; }
+    }
+}
---
+++ XRPL.Core.Domain/Responses/TransactionEntryResponse.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+using XRPL.Core.Domain.Models;
+
+namespace XRPL.Core.Domain.Responses
+{
+    /// <summary>
+    /// Represents a response to a submit response.
+    /// </summary>
+    [DataContract]
+    public class TransactionEntryResponse : ResponseBase<TransactionEntryResult>
+    {
+    }
+
+    /// <summary>
+    /// Represents a result of an <see cref="TransactionEntryResponse"/> object.
+    /// </summary>
+    [DataContract]
+    public abstract class TransactionEntryResult : ResultBase
+    {
+        /// <summary>
+        /// The ledger index of the ledger version the transaction was found in; this is the same as the one from the request.
+        /// </summary>
+        [DataMember(Name = "ledger_index")]
+        public uint LedgerIndex { get; set; }
+
+        /// <summary>
+        /// (May be omitted) The identifying hash of the ledger version the transaction was found in; this is the same as the one from the request.
+        /// </summary>
+        [DataMember(Name = "ledger_hash")]
+        public string? LedgerHash { get; set; }
+
+        /// <summary>
+        /// The transaction metadata, which shows the exact results of the transaction in detail.
+        /// </summary>
+        [DataMember(Name = "metadata")]
+        public TransactionMetadata? Metadata { get; set; }
+
+        /// <summary>
+        /// JSON representation of the Transaction object
+        /// </summary>
+        [DataMember(Name = "tx_json")]
+        public Transaction? TxJson { get; set; }
+    }
+}
