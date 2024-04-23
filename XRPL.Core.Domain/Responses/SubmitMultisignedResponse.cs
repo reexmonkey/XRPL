dc57c60328d6da66b062c10943ee18fb150c2af2
harold william mboua <mbouawilliam@hotmail.com>
2024-04-23 19:22:38 +0200

    Submit Multisigned(Request and response)

---
+++ XRPL.Core.Domain/Responses/SubmitMultisignedResponse.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Serialization;
+
+namespace XRPL.Core.Domain.Responses
+{
+    /// <summary>
+    /// Represents a response to a submit multisigned response.
+    /// </summary>
+    public class SubmitMultisignedResponse : ResponseBase<SubmitMultisignedResult>
+    {
+    }
+
+    /// <summary>
+    /// Represents a result of an <see cref="SubmitMultisignedResponse"/> object.
+    /// </summary>
+    [DataContract]
+    public abstract class SubmitMultisignedResult : ResultBase
+    {
+        /// <summary>
+        /// Code indicating the preliminary result of the transaction, for example tesSuccess
+        /// </summary>
+        [DataMember(Name = "engíne_result")]
+        public string? EngineResult { get; set; }
+
+        /// <summary>
+        /// Numeric code indicating the preliminary result of the transaction, directly correlated to engine_result
+        /// </summary>
+        [DataMember(Name = "engíne_result_code")]
+        public int? EngineResultCode { get; set; }
+
+        /// <summary>
+        /// Human-readable explanation of the preliminary transaction result
+        /// </summary>
+        [DataMember(Name = "engíne_result_message")]
+        public string? EngineResultMessage { get; set; } 
+
+    }
+
+    public sealed class BinarySubmitMultisignedResult : SubmitMultisignedResult
+    {
+        /// <summary>
+        /// The complete transaction in hex string format.
+        /// </summary>
+        [DataMember(Name = "tx_blob")]
+        public string? TxBlob { get; set; }
+    }
+
+    public sealed class JsonSubmitMultisignedResult : SubmitMultisignedResult
+    {
+        /// <summary>
+        /// The complete transaction in JSON format.
+        /// </summary>
+        [DataMember(Name = "tx_json")]
+        public Transaction? TxJson { get; set; }
+    }
+}
