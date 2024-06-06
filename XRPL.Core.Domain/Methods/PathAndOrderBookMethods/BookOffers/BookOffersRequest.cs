a26b2ac3533059f4e061b2d697c68ed0ac6ef7b0
harold william mboua <mbouawilliam@hotmail.com>
2024-06-06 14:15:20 +0200

    Book Offers :)

---
+++ XRPL.Core.Domain/Methods/PathAndOrderBookMethods/BookOffers/BookOffersRequest.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json.Serialization;
+using XRPL.Core.Domain.Interfaces;
+using XRPL.Core.Domain.Models;
+
+namespace XRPL.Core.Domain.Methods.PathAndOrderBookMethods.BookOffers
+{
+    /// <summary>
+    /// Represent a request that retrieves a list of Offers between two currencies,
+    /// also known as an order book. The response omits unfunded Offers and reports
+    /// how much of each remaining Offer's total is currently funded.
+    /// </summary>
+    public class BookOffersRequest : RequestBase<BookOffersParameters>, IExpect<BookOffersResponse>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookOffersRequest"/> class.
+        /// </summary>
+        public BookOffersRequest() : base("book_offers")
+        {
+        }
+    }
+
+    /// <summary>
+    /// Represents the parameters of an <see cref="BookOffersRequest"/> object.
+    /// </summary>
+    public class BookOffersParameters : ParameterBase
+    {
+        ///<summary>
+        ///The asset the account taking the Offer would receive, as a currency without an amount.
+        ///</summary>
+        [JsonPropertyName("taker_gets")]
+        public required TakerGetsCurrency TakerGets { get; set; }
+
+        ///<summary>
+        ///The asset the account taking the Offer would pay, as a currency without an amount.
+        ///</summary>
+        [JsonPropertyName("taker_pays")]
+        public required TakerPaysCurrency TakerPays { get; set; }
+
+        ///<summary>
+        ///A 20-byte hex string for the ledger version to use. (See Specifying Ledgers)
+        ///</summary>
+        [JsonPropertyName("ledger_hash")]
+        public string? LedgerHash { get; set; }
+
+        ///<summary>
+        ///	The ledger index of the ledger to use, or a shortcut string to choose a ledger automatically. (See Specifying Ledgers)
+        ///</summary>
+        [JsonPropertyName("ledger_index")]
+        public uint? LedgerIndex { get; set; }
+
+        ///<summary>
+        ///	The maximum number of Offers to return. The response may include fewer results.
+        ///</summary>
+        [JsonPropertyName("limit")]
+        public uint? Limit { get; set; }
+
+        ///<summary>
+        ///The Address of an account to use as a perspective.
+        ///The response includes this account's Offers even if they are unfunded.
+        ///(You can use this to see what Offers are above or below yours in the order book.)
+        ///</summary>
+        [JsonPropertyName("taker")]
+        public string? Taker { get; set; }
+    }
+
+    /// <summary>
+    /// The type of currency the receiver will get
+    /// </summary>
+    public class TakerGetsCurrency : Asset
+    {
+    }
+
+    /// <summary>
+    /// The type of currency the issuer will pay
+    /// </summary>
+    public class TakerPaysCurrency : Asset 
+    {
+    }
+}
---
+++ XRPL.Core.Domain/Methods/PathAndOrderBookMethods/BookOffers/BookOffersResponse.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json.Serialization;
+using XRPL.Core.Domain.Entries;
+using XRPL.Core.Domain.Models;
+
+namespace XRPL.Core.Domain.Methods.PathAndOrderBookMethods.BookOffers
+{
+    /// <summary>
+    /// Represents a response to a book offer request.
+    /// </summary>
+    public class BookOffersResponse : ResponseBase<BookOffersResult>
+    {
+    }
+
+    /// <summary>
+    /// Represents a result of an <see cref="BookOffersResponse"/> object.
+    /// </summary>
+    public class BookOffersResult : ResultBase
+    {
+        /// <summary>
+        ///(Omitted if ledger_current_index is provided)
+        ///The ledger index of the current in-progress ledger version, which was used to retrieve this information.
+        /// </summary>
+        [JsonPropertyName("ledger_current_index")]
+        public uint? LedegerCurrentIndex { get; set; }
+
+        /// <summary>
+        ///(Omitted if ledger_current_index provided)
+        ///The ledger index of the ledger version that was used when retrieving this data, as requested.
+        /// </summary>
+        [JsonPropertyName("ledger_index")]
+        public uint? LedgerIndex { get; set; }
+
+        /// <summary>
+        ///(May be omitted) The identifying hash of the ledger version that was used when retrieving this data, as requested.
+        /// </summary>
+        [JsonPropertyName("ledger_hash")]
+        public string? LedgerCount { get; set; }
+
+        /// <summary>
+        ///Array of offer objects, each of which has the fields of an Offer object
+        /// </summary>
+        [JsonPropertyName("offers")]
+        public required OfferOfBookOffers[] Offers { get; set; }
+
+    }
+
+    ///<summary>
+    ///Additional members that could be added to the Standard Offer fields.
+    ///</summary>
+    public class OfferOfBookOffers : Offer
+    {
+        ///<summary>
+        ///Amount of the TakerGets currency the side placing the offer has available to be traded.
+        ///(XRP is represented as drops; any other currency is represented as a decimal value.)
+        ///If a trader has multiple offers in the same book, only the highest-ranked offer includes this field.
+        ///</summary>
+        [JsonPropertyName("owner_funds")]
+        public required string OwnerFunds { get; set; }
+
+        ///<summary>
+        ///(Only included in partially-funded offers)
+        ///The maximum amount of currency that the taker can get,
+        ///given the funding status of the offer.
+        ///</summary>
+        [JsonPropertyName("taker_gets_funded")]
+        public TokenAmount? TakerGetsFunded {  get; set; } 
+
+        ///<summary>
+        ///
+        ///</summary>
+        [JsonPropertyName("taker_pays_funded")]
+        public TokenAmount? TakerPaysFunded { get; set; }
+
+        ///<summary>
+        ///The exchange rate, as the ratio taker_pays divided by taker_gets.
+        ///For fairness, offers that have the same quality are automatically taken first-in, first-out.
+        ///(In other words, if multiple people offer to exchange currency at the same rate, the oldest offer is taken first.)
+        ///</summary>
+        [JsonPropertyName("quality")]
+        public required string Quality { get; set; }
+    }
+}
